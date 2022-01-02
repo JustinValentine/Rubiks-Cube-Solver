935bb8a605320600d57a39b661fd1db619040d0b
Justin Valentine <65584395+JustinValentine@users.noreply.github.com>
2022-01-02 16:41:50 -0700

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,7 +1,6 @@
 GRID_COLOR = "#000000"
 EMPTY_CELL_COLOR = "#0d0d0d"
-SCORE_LABEL_FONT = ("Verdana", 20)
-SCORE_FONT = ("Helvetica", 32, "bold")
+BUTTON_FONT = ("Verdana", 20)
 
 CELL_COLORS = {
     "W": "#ebebeb", 
